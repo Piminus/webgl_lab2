e4fa066290a6b0a4d2f9df009982c456c5330b76
yesh0221 <yehor.shulha@netcracker.com>
2022-06-19 15:11:30 +0300

    Coommit

--- server.js
+++ server.js
@@ -1,6 +1,31 @@
 var connect = require('connect');
 var serveStatic = require('serve-static');
+var http = require('http'),
+    fs = require('fs');
 
-connect()
-    .use(serveStatic(__dirname))
-    .listen(8080, () => console.log('Server running on 8080...', __dirname));
+
+fs.readFile('./lab2/index.html', function (err, html) {
+    if (err) {
+        throw err;
+    }
+    http.createServer(function(request, response) {
+        if (request.url === '/') {
+            response.writeHeader(200, {"Content-Type": "text/html"});
+            response.write(html);
+            response.end();
+        }
+        if (request.url === '/shader.gpu') {
+            response.write(fs.readFileSync('./lab2/shader.gpu'));
+            response.end();
+        }
+        if (request.url === '/main.js') {
+            response.write(fs.readFileSync('./lab2/main.js'));
+            response.end();
+        }
+        if (request.url === '/Utils/trackball-rotator.js') {
+            response.write(fs.readFileSync('./lab2//Utils/trackball-rotator.js'));
+            response.end();
+        }
+
+    }).listen(8000);
+});
